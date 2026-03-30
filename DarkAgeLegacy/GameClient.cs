5b1ea65096dff0122ef641c19f56e5c9c0395900
Danyl <danilmimozyra@gmail.com>
2026-03-30 21:29:31 +0200

    Initial commit. Server and GameClient added

---
+++ DarkAgeLegacy/GameClient.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+public class GameClient
+{
+    private TcpClient client;
+    private StreamReader reader;
+    private StreamWriter writer;
+
+    public string Id { get; }
+
+    public GameClient(TcpClient client)
+    {
+        this.client = client;
+
+        reader = new StreamReader(client.GetStream(), Encoding.UTF8);
+        writer = new StreamWriter(client.GetStream(), Encoding.UTF8)
+        {
+            AutoFlush = true
+        };
+
+        Id = Guid.NewGuid().ToString();
+    }
+
+    //posila na server
+    public string Receive()
+    {
+        return reader.ReadLine();
+    }
+
+    //pise do konzole klienta
+    public void Send(string message)
+    {
+        writer.WriteLine(message);
+    }
+
+    public void Close()
+    {
+        client.Close();
+    }
+}
---
+++ DarkAgeLegacy/Server.cs
@@ -0,0 +1,74 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+public class Server
+{
+    private TcpListener listener;
+    private bool running = false;
+
+    //sem bych pridal dictionary vsech prikazu
+
+    public Server(int port)
+    {
+        listener = new TcpListener(IPAddress.Any, port);
+    }
+
+    public void Start()
+    {
+        listener.Start();
+        running = true;
+
+        Console.WriteLine("Server started...");
+
+        while (running)
+        {
+            TcpClient tcpClient = listener.AcceptTcpClient();
+
+            GameClient client = new GameClient(tcpClient);
+
+            Console.WriteLine($"Client connected: {client.Id}");
+
+            Thread thread = new Thread(ClientLoop);
+            thread.Start(client);
+        }
+    }
+    
+    //klasicky klient loop pro kazdeho klenta
+    private void ClientLoop(object obj)
+    {
+        GameClient client = (GameClient)obj;
+
+        client.Send("Welcome to server");
+
+        //v podstate hlavni cast programu
+        try
+        {
+            while (true)
+            {
+                string message = client.Receive();
+
+                if (message == "exit")
+                    break;
+
+                Console.WriteLine($"[{client.Id}] {message}");
+
+                string response = HandleMessage(client, message);
+
+                client.Send(response);
+            }
+        }
+        catch
+        {
+            Console.WriteLine($"Client error: {client.Id}");
+        }
+
+        client.Close();
+        Console.WriteLine($"Client disconnected: {client.Id}");
+    }
+
+    //sem pak prijdou komandy
+    protected virtual string HandleMessage(GameClient client, string message)
+    {
+        return $"Echo: {message}";
+    }
+}
