9a5ca51891707d5f175f24c2c8748afd3251bec5
Pavel Václavek <pavel.vaclavek88@gmail.com>
2026-04-04 15:39:23 +0200

    Added Map and Room class

---
+++ DarkAgeLegacy/Map/Map.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkAgeLegacyServer.Map
+{
+    internal class Map
+    {
+        private Dictionary<int, Room> map;
+        private Random rd;
+
+        public Map()
+        {
+            map = new Dictionary<int, Room>();
+            rd = new Random();
+            LoadMap();
+        }
+
+        private void LoadMap()
+        {
+
+            string filePath = "res/map.txt";
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    String[] roomInfo = line.Split(",");
+                    Room r = new Room(roomInfo[1],
+                            Int32.Parse(roomInfo[2]),
+                            Int32.Parse(roomInfo[3]),
+                            Int32.Parse(roomInfo[4]),
+                            Int32.Parse(roomInfo[5]));
+
+                    map.Add(Int32.Parse(roomInfo[0]), r);
+                }
+            }
+        }
+    }
+}
---
+++ DarkAgeLegacy/Map/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkAgeLegacyServer.Map
+{
+    internal class Room
+    {
+        private String name;
+        private int westRoom;
+        private int northRoom;
+        private int eastRoom;
+        private int southRoom;
+
+        public Room(String name, int westRoom, int northRoom, int eastRoom, int southRoom)
+        {
+            this.name = name;
+            this.westRoom = westRoom;
+            this.northRoom = northRoom;
+            this.eastRoom = eastRoom;
+            this.southRoom = southRoom;
+        }
+    }
+}
